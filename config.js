b5a85703e911a74c12fbf01f0c95def6a6b93ec5
baku171 <pbak@esri.pl>
2017-12-12 09:19:50 +0100

    Zadanie 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '396847866083-p0t2f4402ksvavt8mg1hr29l71p8bu9v.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'CNJNF8akbYO0vBgS1psLTbA5',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
